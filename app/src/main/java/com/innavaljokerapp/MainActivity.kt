6ab2e8a0c7104a905df009a52ee39ce43c41d073
Eduardo <engenedu@hotmail.com>
2022-03-10 18:34:11 -0300

    first commit

---
+++ app/src/main/java/com/innavaljokerapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.innavaljokerapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
